e1480a1b9589fc6c9d8833592981e13daadc2696
Uday P <udayp5277@gmail.com>
2026-08-15 12:19:06 +0530

    Time: 0 ms (100%), Space: 17.9 MB (99.33%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        vector<vector<int>>ans(image.begin(),image.end());
-        queue<pair<pair<int,int>,int>>q;
-        q.push({{sr,sc},image[sr][sc]});
-        ans[sr][sc] = color;
-        int dr[4] = {-1,0,0,1};
-        int dc[4] = {0,1,-1,0};
-        while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int val = q.front().second;
-            q.pop();
-            for(int i = 0 ; i<4 ; i++){
-                int nr = r+dr[i];
-                int nc = c+dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc] == val && ans[nr][nc]!=color){
-                    ans[nr][nc] = color;
-                    q.push({{nr,nc},image[nr][nc]});
-                }
+    void dfs(int r,int c,int initialColor,vector<vector<int>>& image,vector<vector<int>>& ans,int color,int dr[], int dc[]){
+        ans[r][c] = color;
+
+        for(int i = 0 ; i<4 ; i++){
+            int nr = r+dr[i];
+            int nc = c+dc[i];
+            if(nr>=0 && nr<image.size() && nc>=0 && nc<image[r].size() && image[nr][nc] == initialColor && ans[nr][nc]!=color){
+                dfs(nr,nc,initialColor,image,ans,color,dr,dc);
             }
         }
+        
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>>ans = image;
+        int initialColor = image[sr][sc];
+        int dr[4] = {-1,0,0,1};
+        int dc[4] = {0,1,-1,0};
+        dfs(sr,sc,initialColor,image,ans,color,dr,dc);
+
         return ans;
     }
 };
