076c135a3f12b677737fcd436fc43bc56102a5ae
Uday P <udayp5277@gmail.com>
2026-08-24 23:42:54 +0530

    Time: 2 ms (20.86%), Space: 9.5 MB (5.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -11,6 +11,16 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,dp);
+        dp[0][0] = 1;
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(i == 0 && j == 0) continue;
+                int up = (i-1 < 0) ? 0 : dp[i-1][j];
+                int left = (j-1 < 0) ? 0 : dp[i][j-1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
+        // return helper(m-1,n-1,dp);
     }
 };
